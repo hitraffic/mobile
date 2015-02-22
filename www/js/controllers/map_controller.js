c356334fb58e626b3da729072338acb030fdc73b
Kevin Shin <kevnshin@gmail.com>
2015-02-21 16:20:51 -1000

    Make mock data markers appear on map

--- www/js/controllers/map_controller.js
+++ www/js/controllers/map_controller.js
@@ -1,144 +1,109 @@
-angular.module('mobility').controller('MapController',
-  [ '$scope',
-    '$cordovaGeolocation',
-    '$stateParams',
-    '$ionicModal',
-    '$ionicPopup',
-    'LocationsService',
-    'InstructionsService',
-    function(
-      $scope,
-      $cordovaGeolocation,
-      $stateParams,
-      $ionicModal,
-      $ionicPopup,
-      LocationsService,
-      InstructionsService,
-      Incidents
-      ) {
+angular.module('mobility').controller('MapController', function(
+  $scope,
+  $cordovaGeolocation,
+  $stateParams,
+  $ionicModal,
+  $ionicPopup,
+  LocationsService,
+  InstructionsService,
+  Incidents
+  ) {
+    /**
+     * Once state loaded, get put map on scope.
+     */
+    $scope.$on("$stateChangeSuccess", function() {
 
-
-
-      /**
-       * Once state loaded, get put map on scope.
-       */
-      $scope.$on("$stateChangeSuccess", function() {
-
-        $scope.locations = LocationsService.savedLocations;
-        $scope.newLocation;
-
-        if(!InstructionsService.instructions.newLocations.seen) {
-
-          var instructionsPopup = $ionicPopup.alert({
-            title: 'Add Locations',
-            template: InstructionsService.instructions.newLocations.text
-          });
-          instructionsPopup.then(function(res) {
-            InstructionsService.instructions.newLocations.seen = true;
-            });
-
-        }
-
-        $scope.map = {
-          defaults: {
-            tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-            maxZoom: 18,
-            zoomControlPosition: 'bottomleft'
-          },
-          markers : {},
-          events: {
-            map: {
-              enable: ['context'],
-              logic: 'emit'
-            }
+      //Initialize map properties
+      $scope.map = {
+        defaults: {
+          tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+          maxZoom: 18,
+          zoomControlPosition: 'bottomleft'
+        },
+        markers : {},
+        events: {
+          map: {
+            enable: ['context'],
+            logic: 'emit'
           }
-        };
-
-        $scope.goTo(0);
-
+        }
+      };
+      //bring all incidents into scope
+      $scope.incidents = Incidents.all();
+      var incident_num = 1; 
+      $scope.incidents.forEach(function (incident, index, array) {
+        var marker = {
+          lat: incident.lat,
+          lng: incident.lng,
+          message: incident.type + "\nArea: " + incident.area + "\nAddress: " + incident.address
+        }
+        if(marker.lat !== null || marker.lng !== null){
+          $scope.map.markers["Incident" + incident_num] = marker;
+        }
+        incident_num++;
+        console.log($scope.map.markers);
       });
 
-      var Location = function() {
-        if ( !(this instanceof Location) ) return new Location();
-        this.lat  = "";
-        this.lng  = "";
-        this.name = "";
+
+      $scope.goTo();
+
+
+    });
+
+
+
+    /**
+     * Center map on specific saved location
+     * @param locationKey
+     */
+    $scope.goTo = function(locationKey) {
+
+      // var location = LocationsService.savedLocations[locationKey];
+      var location = LocationsService;
+
+      $scope.map.center  = {
+        lat : location.lat,
+        lng : location.lng,
+        zoom : 11
       };
 
-      $ionicModal.fromTemplateUrl('templates/addLocation.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function(modal) {
-          $scope.modal = modal;
-        });
+      // $scope.map.markers[locationKey] = {
+      //   lat:location.lat,
+      //   lng:location.lng,
+      //   message: location.name,
 
-      /**
-       * Detect user long-pressing on map to add new location
-       */
-      $scope.$on('leafletDirectiveMap.contextmenu', function(event, locationEvent){
-        $scope.newLocation = new Location();
-        $scope.newLocation.lat = locationEvent.leafletEvent.latlng.lat;
-        $scope.newLocation.lng = locationEvent.leafletEvent.latlng.lng;
-        $scope.modal.show();
-      });
+      //   focus: true,
+      //   draggable: false
+      // };
 
-      $scope.saveLocation = function() {
-        LocationsService.savedLocations.push($scope.newLocation);
-        $scope.modal.hide();
-        $scope.goTo(LocationsService.savedLocations.length - 1);
-      };
+    };
 
-      /**
-       * Center map on specific saved location
-       * @param locationKey
-       */
-      $scope.goTo = function(locationKey) {
+    /**
+     * Center map on user's current position
+     */
+    // $scope.locate = function(){
 
-        var location = LocationsService.savedLocations[locationKey];
+    //   $cordovaGeolocation
+    //     .getCurrentPosition()
+    //     .then(function (position) {
+    //       $scope.map.center.lat  = position.coords.latitude;
+    //       $scope.map.center.lng = position.coords.longitude;
+    //       $scope.map.center.zoom = 15;
 
-        $scope.map.center  = {
-          lat : location.lat,
-          lng : location.lng,
-          zoom : 11
-        };
+    //       $scope.map.markers.now = {
+    //         lat:position.coords.latitude,
+    //         lng:position.coords.longitude,
+    //         message: "You Are Here",
+    //         focus: true,
+    //         draggable: false
+    //       };
 
-        $scope.map.markers[locationKey] = {
-          lat:location.lat,
-          lng:location.lng,
-          message: location.name,
+    //     }, function(err) {
+    //       // error
+    //       console.log("Location error!");
+    //       console.log(err);
+    //     });
 
-          focus: true,
-          draggable: false
-        };
+    // };
 
-      };
-
-      /**
-       * Center map on user's current position
-       */
-      $scope.locate = function(){
-
-        $cordovaGeolocation
-          .getCurrentPosition()
-          .then(function (position) {
-            $scope.map.center.lat  = position.coords.latitude;
-            $scope.map.center.lng = position.coords.longitude;
-            $scope.map.center.zoom = 15;
-
-            $scope.map.markers.now = {
-              lat:position.coords.latitude,
-              lng:position.coords.longitude,
-              message: "You Are Here",
-              focus: true,
-              draggable: false
-            };
-
-          }, function(err) {
-            // error
-            console.log("Location error!");
-            console.log(err);
-          });
-
-      };
-
-    }]);
+  });
