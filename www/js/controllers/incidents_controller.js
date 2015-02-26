1026b8c564f1ee3aefde0f662ea30dab84c9f38d
Kevin Shin <kevnshin@gmail.com>
2015-02-26 11:50:29 -1000

    Get Map to pull data from GET request. Fix center not in main scope issue

--- www/js/controllers/incidents_controller.js
+++ www/js/controllers/incidents_controller.js
@@ -1,7 +1,10 @@
 angular.module('mobility')
 
 .controller('IncidentsController', function($scope, $mdSidenav, Incidents) {
-  $scope.incidents = Incidents.all();
+  $scope.incidents = null;
+  Incidents.all().then(function (incidents) {
+    $scope.incidents = incidents;
+  });
 
   $scope.remove = function(incident) {
     Incidents.remove(incident);
--- www/js/controllers/map_controller.js
+++ www/js/controllers/map_controller.js
@@ -5,6 +5,7 @@ angular.module('mobility').controller('MapController', function(
   $ionicModal,
   $ionicPopup,
   $compile,
+  $mdSidenav,
   LocationsService,
   InstructionsService,
   Incidents
@@ -19,6 +20,7 @@ angular.module('mobility').controller('MapController', function(
           maxZoom: 18,
           zoomControlPosition: 'bottomleft'
         },
+        center: {},
         markers : {},
         events: {
           map: {
@@ -28,32 +30,62 @@ angular.module('mobility').controller('MapController', function(
         }
       };
 
+      // $scope.controls = {
+      //   custom: []
+      // }
+
+      // var menu_control = L.control();
+      // menu_control.setPosition('topleft');
+      // menu_control.onAdd = function () {
+      //   var className = 'leaflet-menu',
+      //     container = L.DomUtil.create('div', className + 'leaflet-bar');
+      //   return container;
+      // }
+
+      // menu_control.onAdd();
+      // $scope.controls.custom.push(menu_control);
+
+      // L.easyButton("fa fa-bars",
+      //   function (argument) {
+      //     // body...
+      //   },
+      //   "BUTTON",
+      //   "#map"
+      // );
+
+
       //bring all incidents into scope
       //FIX: Create function to continually check for updates to HITraffic API
-      $scope.incidents = Incidents.all();
-      var incident_num = 1; 
-      $scope.incidents.forEach(function (incident, index, array) {
-        var html = "<h5 class='incident_type'>" + incident.type + "</h5>"
-          + "<p class='marker_text'>" + incident.date + "</p>"
-          + "<p class='marker_text'>" + incident.address + "</p>"
-          + "<p class='marker_text'>" + incident.area + "</p>";
-          // + "<a class='marker_text zoom' on-tap='focusHere(incident)'>Zoom Here</a>";
-        var marker = {
-          lat: incident.lat,
-          lng: incident.lng,
-          // getMessageScope: function () { return $scope; },
-          message: html,
-          // compileMessage: true
-        }
-        //Only add to list of markers if there are coordinates
-        if(marker.lat !== null || marker.lng !== null){
-          $scope.map.markers["Incident" + incident_num] = marker;
-        }
-        incident_num++;
+      $scope.incidents = [];
+      Incidents.all().then(function (incidents) {
+        // console.log(incidents);
+        $scope.incidents = incidents;
+        var incident_num = 1; 
+        $scope.incidents.forEach(function (incident, index, array) {
+          var html = "<h5 class='incident_type'>" + incident.type + "</h5>"
+            + "<p class='marker_text'>" + incident.date + "</p>"
+            + "<p class='marker_text'>" + incident.address + "</p>"
+            + "<p class='marker_text'>" + incident.area + "</p>";
+            // + "<a class='marker_text zoom' on-tap='focusHere(incident)'>Zoom Here</a>";
+          var marker = {
+            lat: incident.lat,
+            lng: incident.lng,
+            // getMessageScope: function () { return $scope; },
+            message: html,
+            // compileMessage: true
+          }
+          //Only add to list of markers if there are coordinates
+          if(marker.lat !== null || marker.lng !== null){
+            $scope.map.markers["Incident" + incident_num] = marker;
+          }
+          incident_num++;
+        });
+        $scope.focusHere(LocationsService);
       });
 
+
       //Go to initial map
-      $scope.focusHere(LocationsService);
+      // $scope.focusHere(LocationsService);
 
     });
 
@@ -106,5 +138,13 @@ angular.module('mobility').controller('MapController', function(
     //     });
 
     // };
+
+  $scope.toggleList = function() {
+    $mdSidenav('left').toggle();
+  };
+
+  $scope.closeList = function() {
+    $mdSidenav('left').close();
+  };
 
   });
